5dd74ccf800b114cb28921002684ccc3e1b0c85d
Ian Noble <ian+github@iannoble.co.uk>
2017-10-08 11:16:26 +0100

    Add basic pester test.

---
+++ functions/Wait-FileContent.Tests.ps1
@@ -0,0 +1,12 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Wait-FileContent" {
+    It "It waits for one or more regex patterns to appear in a file and supports file rotation." {
+        $path = "$here\tests\assets\sample-data\log.log"
+        $search = @{'FinalRelease' = 'Completed'}
+        $scriptblock = [scriptblock]::Create('$null = Add-Content $path "FinalRelease"')
+        Wait-FileContent -path $path -regexpatterns $search -script $scriptblock | Should Be 'Completed'
+    }
+}
